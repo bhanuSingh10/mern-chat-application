5972cb0a8949d07addd013c357a0ac5578444ec1
bhanuSingh10 <bs426681@gmail.com>
2025-04-06 18:13:48 +0530

    edit socket for vercel

--- server/socket.js
+++ server/socket.js
@@ -5,7 +5,7 @@ import Channel from "./models/ChannelModel.js";
 const setupSocket = (server) => {
   const io = new SocketIOServer(server, {
     cors: {
-      origin: process.env.ORIGIN,
+      origin: "https://mern-chat-application-ecru.vercel.app",
       methods: ["GET", "POST"],
       credentials: true
     }
